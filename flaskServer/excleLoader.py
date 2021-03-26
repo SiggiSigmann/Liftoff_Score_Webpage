09113cb84fe3aa4a6c228ef5c2407d9d305f7af2
SiggiSigmann <siggisigmann@googlemail.com>
2021-03-26 12:42:46 +0000

    add regex check for values in import

--- flaskServer/excleLoader.py
+++ flaskServer/excleLoader.py
@@ -70,13 +70,20 @@ class ExcelLoader():
 								time = user.value.rstrip()
 								username = username_idx[index]
 
+								matched = re.match("[a-zA-z0-9]{3,25}", username)
+								if not bool(matched):
+									continue
+
 								ids = self.db.get_map_track_id(map_name, track_name)
 								if len(ids) < 1:
 									continue
-								print(ids, file=sys.stderr)
 
-								print(str(self.drone_id[dronename]) + " " + str(ids[0]) +" "+ str(ids[1])+" "+str(self.user_id[username])+ " "+time, file=sys.stderr)
+								matched = re.match("[0-9]{2}:[0-9]{2}:[0-9]{3}", time)
+								if not bool(matched):
+									continue
 								self.db.add_new_result(ids[0], ids[1], str(self.user_id[username]), self.drone_id[dronename], time)
+							
+						
 						index += 1
 
 		#self.db.add_new_result(mapid, trackid, userid, droneid, resulttimestamp):
@@ -85,12 +92,18 @@ class ExcelLoader():
 
 	def _make_shure_user_exists(self, username, color):
 		if not username in self.user_list:
+			matched = re.match("[a-zA-z0-9]{3,25}", username)
+			if not bool(matched):
+				return
 			print("user not exis: " + username + " ("+color+")", file=sys.stderr)
 			self.db.add_new_user(username,color)
 			self.user_list.append(username)
 
 	def _make_shure_drone_exists(self, dronename):
 		if not dronename in self.drone_list:
+			matched = re.match("[a-zA-z0-9]{3,25}", dronename)
+			if not bool(matched):
+				continue
 			print("drone not exis: " + dronename, file=sys.stderr)
 			self.db.add_new_drone(dronename)
 			self.drone_list.append(dronename)
--- flaskServer/server.py
+++ flaskServer/server.py
@@ -110,10 +110,10 @@ def create_result():
     if not (len(drones) >= droneid and 0 < droneid):
         success = 0
 
-    #print("check file", file=sys.stderr)
-    #matched = re.match("[0-9]{2}:[0-9]{2}:[0-9]{3}", resulttimestamp)
-    #if not bool(matched):
-    #    success = 0
+
+    matched = re.match("[0-9]{2}:[0-9]{2}:[0-9]{3}", resulttimestamp)
+    if not bool(matched):
+        success = 0
     
     #==>
     print("insert", file=sys.stderr)
@@ -158,8 +158,6 @@ def create_user():
 ### imex ########################################
 @app.route("/imex", methods=["GET"])
 def imex_get():
-    loader.loadFile(os.path.join(app.config['UPLOAD_FOLDER'], "sam.xlsx"))
-
     return render_template('imex.html', active="imex")
 
 def allowed_file(filename):
@@ -170,31 +168,23 @@ def allowed_file(filename):
 def upload_file():
     # check if the post request has the file part
     if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
+        return render_template('imex.html', active="imex", error=-1)
     file = request.files['file']
     # if user does not select file, browser also
     # submit an empty part without filename
     if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
+        return render_template('imex.html', active="imex", error=-2)
     if file and allowed_file(file.filename):
+        #file was uploaded
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+        loader.loadFile(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
        
-        return redirect(url_for('upload_file',
-                                filename=filename))
-    
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+        return render_template('imex.html', active="imex", error=0)
 
 ### / ##########################################
 @app.route("/", methods=["GET"])
